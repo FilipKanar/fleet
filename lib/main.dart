5bad09c200d860beb8dff25aa5ca34f2a11dd492
Borsuk <antena309@gmail.com>
2023-02-01 09:26:07 +0100

    Authentication UI with firebase_ui_auth, routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  FirebaseUIAuth.configureProviders(
+      [
+        EmailAuthProvider(),
+      ]
+  );
   runApp(const MyApp());
 }
 
@@ -10,6 +17,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var providers = [EmailAuthProvider()];
+
+    return MaterialApp(
+      initialRoute: FirebaseAuth.instance.currentUser == null ? '/sign-in' : '/home',
+      routes: {
+        '/sign-in' : (context) => SignInScreen(
+          providers: providers,
+          actions: [
+            AuthStateChangeAction<SignedIn>((context, _) {
+              Navigator.of(context).pushReplacementNamed('/home');
+            }),
+          ],
+        ),
+        '/profile' : (context) => ProfileScreen(
+          providers: providers,
+        ),
+      },
+    );
   }
 }
